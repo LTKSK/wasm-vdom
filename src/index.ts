6eb65e6601827d9b38c7759488936bd145d65003
LTKSK <ltksk0714@gmail.com>
2020-06-24 21:40:03 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import("../wasm/pkg")
   .then((wasm) => {
     console.log(wasm.greeting());
     const container = document.getElementById("container");
-    wasm.render(container);
+    console.log(wasm.render(container));
   })
   .catch(console.error);
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,28 +1,50 @@
+use std::collections::HashMap;
 use wasm_bindgen::prelude::*;
-use web_sys::{
-    CanvasRenderingContext2d, Document, Element, HtmlElement, ImageData, Request, RequestInit,
-    RequestMode, Response, Window,
-};
+use web_sys::{Document, Element, HtmlElement, Window};
 
 #[wasm_bindgen]
 pub fn greeting() -> String {
     "hello world".to_string()
 }
 
-//#[derive(Debug)]
-//pub struct Props {
-//    title: String,
-//    children: Element
-//}
-//
-//#[derive(Debug)]
-//pub struct Element {
-//    type: String,
-//    props: Props,
-//}
+#[derive(Debug)]
+enum NodeType {
+    TEXT_ELEMENT,
+}
+
+#[derive(Debug)]
+enum NodeValue {
+    number(f32),
+    string(String),
+    boolean(bool),
+}
+
+type Props = HashMap<String, NodeValue>;
+
+#[derive(Debug)]
+struct VNode {
+    node_type: NodeType,
+    props: Props,
+    //realDOMへの参照?
+    //children?
+    value: String,
+    children: Vec<VNode>,
+}
+
+fn create_element(node_type: NodeType, props: Props, children: Vec<VNode>) -> Option<VNode> {
+    Some(VNode {
+        node_type,
+        props,
+        value: "hoge".to_string(),
+        children: vec![],
+    })
+}
 
 #[wasm_bindgen]
 pub fn render(container: &JsValue) -> Result<(), JsValue> {
     let document = web_sys::window().unwrap().document().unwrap();
+    // containerのdomは引数として既にある状態
+    // propsの情報を、dom[name] = valueとして詰めていかないといけない
+    // containerに対して、childrenをappendChildしていく必要がある
     Ok(())
 }
