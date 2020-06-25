059687963502f6df812aaf4212b93fc6c9d57165
LTKSK <ltksk0714@gmail.com>
2020-06-25 20:42:00 +0900

    update

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -9,6 +9,7 @@ pub fn greeting() -> String {
 
 #[derive(Debug)]
 enum NodeType {
+    VNODE,
     TEXT_ELEMENT,
 }
 
@@ -27,24 +28,45 @@ struct VNode {
     props: Props,
     //realDOMへの参照?
     //children?
-    value: String,
+    value: Option<String>,
     children: Vec<VNode>,
 }
 
 fn create_element(node_type: NodeType, props: Props, children: Vec<VNode>) -> Option<VNode> {
+    // まずは決め打ちでElementを作って、これをrender関数にぶん投げたら
+    // domができる
+    // という形を実装して、その後ちゃんとした関数にする
+    // それもできたら差分チェックを作る
+    // 最初はdivとtextだけ
     Some(VNode {
         node_type,
         props,
-        value: "hoge".to_string(),
-        children: vec![],
+        value: None,
+        children: vec![VNode {
+            node_type: NodeType::VNODE,
+            props: Props::new(),
+            value: Some("testだよん".to_string()),
+            children: vec![],
+        }],
     })
 }
 
+fn render_dom(contaner: &JsValue, root: VNode) -> Result<(), JsValue> {
+    // 受け取ったVNodeをもとに、DOMツリーを構築する
+    // create_element("div", Props::new("id".to_string()): })
+    Ok(())
+}
+
 #[wasm_bindgen]
-pub fn render(container: &JsValue) -> Result<(), JsValue> {
+pub fn render(id: &str) -> Result<(), JsValue> {
     let document = web_sys::window().unwrap().document().unwrap();
     // containerのdomは引数として既にある状態
     // propsの情報を、dom[name] = valueとして詰めていかないといけない
     // containerに対して、childrenをappendChildしていく必要がある
+    // create_element("div", Props::new("id".to_string()): })
+    let node = document.create_element("div")?;
+    node.set_inner_html("append test");
+    let container = document.get_element_by_id(id).expect("no id `container`");
+    container.append_child(&node)?;
     Ok(())
 }
